81297a261a4217e373838288dfe45065ab7b0725
Skygazer1111 <pr9348@srmist.edu.in>
2026-02-20 10:55:39 +0530

    UC1: Add welcome message module

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -7,6 +7,7 @@ public class PalindromeCheckerApp {
         Usecase1();
         Usecase2();
         Usecase3();
+        Usecase4();
     }
 
     static void Usecase1() {
@@ -57,6 +58,37 @@ public class PalindromeCheckerApp {
         } else {
             System.out.println("UC3 Result: \"" + input + "\" is NOT a Palindrome (Reverse Method)");
         }
+    }    
+    static void Usecase4() {
+
+        String input = "radar";
+
+        // Convert to char array
+        char[] characters = input.toCharArray();
+
+        int start = 0;
+        int end = characters.length - 1;
+        boolean isPalindrome = true;
+
+        // Two-pointer technique
+        while (start < end) {
+
+            if (characters[start] != characters[end]) {
+                isPalindrome = false;
+                break;
+            }
+
+            start++;
+            end--;
+        }
+
+        if (isPalindrome) {
+            System.out.println("UC4 Result: \"" + input + "\" is a Palindrome (Two Pointer Method)");
+        } else {
+            System.out.println("UC4 Result: \"" + input + "\" is NOT a Palindrome (Two Pointer Method)");
+        }
+
+        System.out.println("--------------------------------------");
     }    
 
 }

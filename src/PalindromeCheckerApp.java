561363e14d07e95657028a06849390c8e9fecc9e
Skygazer1111 <pr9348@srmist.edu.in>
2026-02-27 08:58:38 +0530

    UC10:Case-Insensitive & Space-Ignored Palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -32,6 +32,7 @@ public class PalindromeCheckerApp {
         Usecase7();
         Usecase8();
         Usecase9();
+        Usecase10();
     }
 
     static void Usecase1() {
@@ -300,6 +301,30 @@ static void Usecase9() {
 
         // Recursive Call
         return check(s, start + 1, end - 1);
+    }
+     static void Usecase10() {
+
+        String input = "A man a plan a canal Panama";
+
+        // Normalize string (remove spaces & symbols, convert to lowercase)
+        String normalized = input.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+
+        boolean isPalindrome = true;
+
+        for (int i = 0; i < normalized.length() / 2; i++) {
+
+            if (normalized.charAt(i) !=
+                normalized.charAt(normalized.length() - 1 - i)) {
+
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        System.out.println("UC10 Input: " + input);
+        System.out.println("Normalized: " + normalized);
+        System.out.println("Is Palindrome? : " + isPalindrome);
+        System.out.println("--------------------------------------");
     }
     
 

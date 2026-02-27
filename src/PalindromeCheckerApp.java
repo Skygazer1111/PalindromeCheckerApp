47648dbf63ebb4883429f571212a7ca3a504af2e
Priyan <pr9348@srmist.edu.in>
2026-02-27 10:09:43 +0530

    only usecase 3 code present
    Removed Usecase1 and Usecase2 methods from the main application.

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -4,40 +4,7 @@ public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        Usecase1();
-        Usecase2();
         Usecase3();
-    }
-
-    static void Usecase1() {
-        System.out.println("Welcome to Palindrome Checker Management System");
-        System.out.println("Version 1.0");
-        System.out.println("System initialized Successfully");
-    }
-
-    static void Usecase2() {
-
-        String input = "madam";
-        char[] test = input.toCharArray();
-
-        int end = input.length() - 1;   // renamed from n (safer)
-        boolean isPalindrome = true;    // better approach than j=0/1
-
-        for (int i = 0; i < input.length() / 2; i++) {
-
-            if (test[i] != test[end]) {
-                isPalindrome = false;
-                break;
-            }
-
-            end--;
-        }
-
-        if (isPalindrome) {
-            System.out.println("The String is palindrome");
-        } else {
-            System.out.println("The String is not palindrome");
-        }
     }
     static void Usecase3() {
 
